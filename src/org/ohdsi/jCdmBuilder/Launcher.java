a0fdea1b00b2c39d7f514cc9e1ceef32a7cae66e
schuemie <schuemie@ohdsi.org>
2015-05-22 15:19:40 +0800

    Added launcher that sets VM heap space.

---
+++ src/org/ohdsi/jCdmBuilder/Launcher.java
@@ -0,0 +1,23 @@
+package org.ohdsi.jCdmBuilder;
+
+
+public class Launcher {
+
+	private final static int	MIN_HEAP	= 2048;
+
+	public static void main(String[] args) throws Exception {
+
+		float heapSizeMegs = (Runtime.getRuntime().maxMemory() / 1024) / 1024;
+
+		if (heapSizeMegs > MIN_HEAP) {
+			System.out.println("Launching with current VM");
+			JCdmBuilderMain.main(args);
+		} else {
+			System.out.println("Starting new VM");
+			String pathToJar = JCdmBuilderMain.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
+			ProcessBuilder pb = new ProcessBuilder("java", "-Xmx" + MIN_HEAP + "m", "-classpath", pathToJar,
+					" org.ohdsi.jCdmBuilder.JCdmBuilderMain");
+			pb.start();
+		}
+	}
+}
